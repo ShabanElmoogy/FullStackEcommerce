a4d85d49c1f11c7eeb035e4090b64d2009676db7
ShabanElmoogy <shaban.cec@gmail.com>
2025-08-19 10:47:11 +0300

    Products Router Setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import productRoutes from './routes/products';
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,8 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!!!!!');
 });
+
+app.use('/products',productRoutes);
 
 app.listen(port,() => {
   console.log('Server is running on port 3000');
---
+++ api/src/routes/products/index.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/',(req,res) => {
+    res.send('Hello World!');
+});
+
+router.get('/:id',(req,res) => {
+    res.send('Hello World!');
+});
+
+router.post('/',(req,res) => {
+    res.send('Hello World!');
+});
+
+router.put('/:id',(req,res) => {
+    res.send('Hello World!');
+});
+
+export default router;
